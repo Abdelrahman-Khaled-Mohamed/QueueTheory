ae57169bdf5188af90c03aec16519ce579e81292
Abdelrahman Khaled <abdelrahman12_6_2000@hotmail.com>
2021-12-17 04:01:24 +0200

    Add files via upload

---
+++ src/javaapplication85/JavaApplication85.java
@@ -0,0 +1,8 @@
+package javaapplication85;
+public class JavaApplication85 {
+    public static void main(String[] args) {
+        StartScreen screen = new StartScreen();
+        screen.setLocationRelativeTo(null);
+        screen.setVisible(true);
+    }
+}
---
+++ src/javaapplication85/StochasticQS.java
@@ -0,0 +1,8 @@
+package javaapplication85;
+abstract class StochasticQS {
+    public abstract double getL();
+    public abstract double getLq();
+    public abstract double getW();
+    public abstract double getWq();
+    public abstract double pOfN(int n);
+}
